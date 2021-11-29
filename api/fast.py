3716e5397e3561ad647b6961a4a5d5d65a2819c9
Cyril Warde <cyrillewarde@gmail.com>
2021-11-29 17:46:31 +0100

    updates

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import base64
+import numpy as np
+
+from pydantic import BaseModel
+from gaiacare_front.predict import Predict
+
+class Image(BaseModel):
+    image: str
+    height: int
+    width: int
+    channel: int
 
 app = FastAPI()
 
@@ -12,5 +23,17 @@ app.add_middleware(
 )
 
 @app.get("/")
-def index():
-    return {"greeting": "Hello world"}
+def home():
+    return {"greeting": "Welcome to GaiaCare API"}
+
+@app.post("/predict")
+def predict_class(Img: Image):
+    #decode image
+    decoded = base64.b64decode(bytes(Img.image, 'utf-8'))
+    decoded = np.frombuffer(decoded, dtype='uint8')
+    decoded = decoded.reshape(Img.height, Img.width, Img.channel)
+    #predict class
+    predictor = Predict()
+    prediction = predictor.predict_class(decoded)
+    print(prediction)
+    return prediction
--- gaiacare_front/predict.py
+++ gaiacare_front/predict.py
@@ -4,11 +4,10 @@ from tensorflow.keras.applications.vgg16 import preprocess_input
 import pandas as pd
 import numpy as np
 
-
 class Predict():
     def __init__(self,
-                 model='gaiacare-front/models/solution1_800-1000_no_datagen',
-                 class_index='gaiacare-front/models/class.csv'):
+                 model='gaiacare_front/models/solution1_800-1000_no_datagen',
+                 class_index='gaiacare_front/models/class.csv'):
         #self.image = np.expand_dims(self.load_and_preprocess_image(image_path), axis=0)
         self.model = models.load_model(model)
         self.class_index = pd.read_csv(class_index).set_index(
@@ -19,9 +18,9 @@ class Predict():
         array = img_to_array(img)
         return preprocess_input(array)
 
-    def predict_class(self, image_path):
-        img = load_img(image_path, target_size=[256, 256])
-        array = img_to_array(img)
+    def predict_class(self, array):
+        #img = load_img(image_path, target_size=[256, 256])
+        #array = img_to_array(img)
         preproc_img = preprocess_input(array)
 
         return list(self.class_index.keys())[list(
@@ -29,9 +28,8 @@ class Predict():
                 np.argmax(
                     self.model.predict(np.expand_dims(preproc_img, axis=0))))]
 
-
 if __name__ == "__main__":
 
     pred = Predict()
 
-    print(pred.predict_class('gaiacare-front/tests/test.JPG'))
+    print(pred.predict_class('gaiacare_front/tests/test.JPG'))
