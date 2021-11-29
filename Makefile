1b5713e924cbd77dc2d25b58be490b6886796a6e
Walid Marouf <walid.marouf@gmail.com>
2021-11-29 12:26:32 +0100

    makefile, app.py

--- Makefile
+++ Makefile
@@ -56,3 +56,23 @@ pypi:
 
 run_api:
 	uvicorn api.fast:app --reload  # load web server with code autoreload
+
+
+deploy_heroku:
+	-@git push heroku master
+	-@heroku ps:scale web=1
+
+# ----------------------------------
+#    LOCAL INSTALL COMMANDS
+# ----------------------------------
+install:
+	@pip install . -U
+
+clean:
+	@rm -fr */__pycache__
+	@rm -fr __init__.py
+	@rm -fr build
+	@rm -fr dist
+	@rm -fr *.dist-info
+	@rm -fr *.egg-info
+	-@rm model.joblib
---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+'''
+# TaxiFareModel front
+'''
+
+st.markdown('''
+Remember that there are several ways to output content into your web page...
+
+Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+''')
+'''
+## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+
+1. Let's ask for:
+- date and time
+- pickup longitude
+- pickup latitude
+- dropoff longitude
+- dropoff latitude
+- passenger count
+'''
+'''
+## Once we have these, let's call our API in order to retrieve a prediction
+
+See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+
+🤔 How could we call our API ? Off course... The `requests` package 💡
+'''
+
+url = 'https://taxifare.lewagon.ai/predict'
+
+if url == 'https://taxifare.lewagon.ai/predict':
+
+    st.markdown(
+        'Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...'
+    )
+'''
+
+2. Let's build a dictionary containing the parameters for our API...
+
+3. Let's call our API using the `requests` package...
+
+4. Let's retrieve the prediction from the **JSON** returned by the API...
+
+## Finally, we can display the prediction to the user
+'''
